3ff074eca461258d1b7db152eb4ab9729313bdf3
Lucasricartealves <165337836+Lucasricartealves@users.noreply.github.com>
2024-10-17 18:50:42 -0300

    Add files via upload

---
+++ colecoes2.py
@@ -0,0 +1,82 @@
+#trabalhando com Tuplas
+diasDaSemana = (
+  "Domingo",
+  "Segunda-Feira",
+  "Terça-Feira",
+  "Quarta-Feira",
+  "Quinta-Feira",
+  "Sexta-Feira",
+  "Sábado",
+  
+)
+print(diasDaSemana)
+
+#O acesso a itens é igual ao Array (listas)
+print(diasDaSemana[0])
+print(diasDaSemana[-1]) #ultimo item
+
+#Loop em uma tupla - igual ao Array
+for umDia in diasDaSemana:
+    print(umDia)
+
+#Tuplas nao podem ser modificadas
+#diasDaSemana[0] = "Primeira-feira"
+#essa tentativa gerará erro
+
+#medindo tamanho de uma tupla - igual ao Array
+print(len(diasDaSemana))
+
+#verificando ocorrencias em uma tupla
+print(diasDaSemana.__contains__("Sábado"))
+
+
+#trabalhando com dicionario e o par [Chave : Valor]
+capitais = {
+  "Goiás" : "Santa Rita",
+  "Santa Catarina" : "Florianópolis",
+  "Bahia" : "Salvador"
+}
+print(capitais)
+
+#Acessando um valor em um dicionario
+print(capitais["Bahia"])
+
+#acessando todas as chaves
+print(capitais.keys())
+
+#acessando todos os valores
+print(capitais.values())
+
+#modificando valores 
+capitais["Goiás"] = "Goiânia"
+print(capitais)
+
+#adicionando valores
+capitais["Maranhão"] = "São Luís"
+print(capitais)
+
+#removendo uma chave e um valor
+capitais.pop("Maranhão")
+print(capitais)
+
+#verificando se o dicionario tem uma chave especifica
+print(capitais.__contains__("Bahia"))
+
+
+
+#REVISÃO PROVA ====================================
+
+ValorParaIncrementarA = -1
+while ValorParaIncrementarA < 6:
+  ValorParaIncrementarA += 1
+  if ValorParaIncrementarA == 3:
+    continue
+  print(ValorParaIncrementarA)
+
+
+for _ in range (10):
+  print("akon")
+
+
+
+
